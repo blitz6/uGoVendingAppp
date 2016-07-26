97fbad391473cf7020bdfc316d600bad2b34309f
sapiah <sachinappiah20@gmail.com>
2016-07-26 14:59:19 -0400

    Added Date Filter system for previous orders data
    included filter for export as well

--- app/src/main/java/com/ugosmoothie/ugovendingapp/Admin/AdministratorActivity.java
+++ app/src/main/java/com/ugosmoothie/ugovendingapp/Admin/AdministratorActivity.java
@@ -8,8 +8,10 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.ugosmoothie.ugovendingapp.Data.Purchase;
@@ -18,6 +20,10 @@ import com.ugosmoothie.ugovendingapp.R;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -32,6 +38,9 @@ public class AdministratorActivity extends AppCompatActivity {
         final Button quit = (Button) findViewById((R.id.quit_button));
         final Button close = (Button) findViewById((R.id.close_button));
         final Button export = (Button) findViewById((R.id.export_button));
+        final Button filter = (Button) findViewById((R.id.filter_list_tag));
+        final EditText start_Date = (EditText) findViewById((R.id.start_tag));
+        final EditText end_Date = (EditText) findViewById((R.id.end_tag));
 
         quit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,43 +62,96 @@ public class AdministratorActivity extends AppCompatActivity {
             }
         });
 
-        List<Purchase> purchases = Purchase.listAll(Purchase.class);
+        filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(start_Date.getText() != null && end_Date.getText() != null){
+                    String start = start_Date.getText().toString();
+                    String end = end_Date.getText().toString();
+                    filter_list(start, end);
+                }
+            }
+        });
+
+        export.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(start_Date.getText() != null && end_Date.getText() != null){
+                    String start = start_Date.getText().toString();
+                    String end = end_Date.getText().toString();
+                    export_list(start, end);
+                }
+            }
+        });
+    }
+
+    public void export_list(String Start_date, String End_date){
+        try {
+            FileOutputStream fos = new FileOutputStream(getStorageDir());
+
+            Date startDate = new Date(Start_date);
+            Date endDate = new Date(End_date);
+
+            try {
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+                startDate = formatter.parse(Start_date);
+                endDate = formatter.parse(End_date);
+            } catch (ParseException ex) {
+                Log.e("Ugo", ex.getMessage());
+            }
+
+            List<Purchase> purchases = Purchase.find(Purchase.class, "Timestamp >= ? and Timestamp <= ?", startDate.getTime() + " ", endDate.getTime() + " ");
+            fos.write("uGoPurchase Log".getBytes());
+            for (Purchase value : purchases) {
+                fos.write(value.getExportString().getBytes());
+            }
+
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void filter_list(String Start_date, String End_date){
+
+        Date startDate = new Date(Start_date);
+        Date endDate = new Date(End_date);
+
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+            startDate = formatter.parse(Start_date);
+            endDate = formatter.parse(End_date);
+        } catch (ParseException ex) {
+            Log.e("Ugo", ex.getMessage());
+        }
+
+        List<Purchase> purchases = Purchase.find(Purchase.class, "Timestamp >= ? and Timestamp <= ?", startDate.getTime() + " ", endDate.getTime() + " ");
 
         String[] array = new String[purchases.size()];
         int index = 0;
         for (
                 Purchase value
                 : purchases)
-
         {
             array[index] = value.getDisplayString();
             index++;
         }
-
         final ListView listview = (ListView) findViewById(R.id.purchase_listview);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, array);
-
         listview.setAdapter(adapter);
+    }
 
-        export.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    FileOutputStream fos = openFileOutput("Purchases", Context.MODE_APPEND | Context.MODE_WORLD_READABLE);
-                    List<Purchase> purchases = Purchase.listAll(Purchase.class);
+    public File getStorageDir() {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS), "Purchases.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException ioe) {
+            Log.e("uGoSmoothie", "Directory not created");
+        }
 
-                    for (Purchase value : purchases) {
-                        fos.write(value.getDisplayString().getBytes());
-                    }
-
-                    fos.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-
+        return file;
     }
 }
